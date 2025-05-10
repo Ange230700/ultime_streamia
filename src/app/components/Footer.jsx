8ded6340cf1dd7c2362f2d19407201b4e6ca39bb
Ange230700 <kouakouangeericstephane@gmail.com>
2025-05-10 13:13:31 +0200

    feat: 🎸  implement footer part 2

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -7,17 +7,21 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer>
-      <p>
-        Check out the source code on{" "}
+    <footer
+      className="p-4 text-center text-2xl"
+      style={{ backgroundColor: "var(--surface-section)" }}
+    >
+      <div className="flex items-center justify-center gap-2">
+        <span>Check out the source code on</span>
         <Link
           href="https://github.com/Ange230700/ultime_streamia"
           target="_blank"
           rel="noopener noreferrer"
+          className="font-semibold hover:underline"
         >
           GitHub
         </Link>
-      </p>
+      </div>
     </footer>
   );
 }
